fa3cfa3870dd0103fc0e954eff357f5daa39805b
cpertile <c.pertile@icloud.com>
2021-07-22 00:44:51 -0300

    #1 adding schemas and resolvers to server file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
-const { ApolloServer } = require('apollo-server');
-const userSchema = require('./schemas/user.gql');
-const userResolvers = require('./resolvers/userResolver');
+const { ApolloServer } = require('apollo-server')
+const { mergeTypeDefs } = require('graphql-tools')
+const userSchema = require('./schemas/userSchema.gql')
+const placeSchema = require('./schemas/placeSchema.gql')
+const agencySchema = require('./schemas/agencySchema.gql')
+const userResolvers = require('./resolvers/userResolver')
+const placeResolvers = require('./resolvers/placeResolver')
+const agencyResolvers = require('./resolvers/agencyResolver')
 
 const server = new ApolloServer({
-	typeDefs: [userSchema],
-	resolvers: [userResolvers]
-});
+	typeDefs: mergeTypeDefs([userSchema, placeSchema, agencySchema]),
+	resolvers: [userResolvers, placeResolvers, agencyResolvers]
+})
 
 server.listen().then(({ url }) => {
 	console.log(`=> Server ready at ${url}`);
